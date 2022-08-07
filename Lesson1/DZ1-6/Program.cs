40394255b9e4fa69a22ac7dae6f1192ac9ba1c10
AAA <shurozka333@mail.com>
2022-08-07 18:38:15 +0300

    DZ1-6

---
+++ Lesson1/DZ1-6/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.WriteLine("Введите число ");
+int number = int.Parse(Console.ReadLine());
+
+Console.WriteLine("Число четное?");
+if (number%2 == 0)
+{    
+    Console.WriteLine("да");
+}
+else 
+{    
+    Console.WriteLine("нет");
+}
