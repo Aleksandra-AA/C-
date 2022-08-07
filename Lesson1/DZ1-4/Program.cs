1414b30cfe7faf99c7963d9ca7098d00a20ae228
AAA <shurozka333@mail.com>
2022-08-07 18:26:21 +0300

    DZ1-4

---
+++ Lesson1/DZ1-4/Program.cs
@@ -0,0 +1,37 @@
+﻿// Нахождение наибольшего числа из трех
+
+Console.WriteLine("Введите число А");
+int A = int.Parse(Console.ReadLine());
+
+Console.WriteLine("Введите число В");
+int B = int.Parse(Console.ReadLine());
+
+Console.WriteLine("Введите число C");
+int C = int.Parse(Console.ReadLine());
+    
+if (A > B && A > C)
+{ 
+    Console.WriteLine("max:" + A);
+} 
+
+if (B > A && B > C)
+{
+    Console.WriteLine("max:" + B);
+}
+
+  if (C > A && C > B)
+{
+    Console.WriteLine("max:" + C);
+}
+
+
+
+
+
+
+
+
+
+
+
+
