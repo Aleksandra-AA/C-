1d28cd24755e1c956bec5cb06804770f9743f206
AAA <shurozka333@mail.com>
2022-08-09 23:35:23 +0300

    add Task2-1

---
+++ Lesson2/DZ2-10/Program.cs
@@ -0,0 +1,15 @@
+﻿
+
+//Console.WriteLine("Введите трехзначное число ");
+
+//int[] array = new int[] { int.Parse(Console.ReadLine()) };
+
+
+
+//for (int i = 0; i < array.Length; i++) ;
+
+//int index = 1;
+
+Console.WriteLine(array[1]);
+
+
---
+++ Lesson2/Task2-1/Program.cs
@@ -0,0 +1,7 @@
+﻿Console.Clear();
+int number = new Random().Next(100, 1000);
+
+Console.WriteLine("Случайное трёхзначное число " + number);
+string str = number.ToString();
+
+Console.WriteLine(str[0].ToString() + str[2].ToString());
