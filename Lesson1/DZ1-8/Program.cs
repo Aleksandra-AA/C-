d80426cdf09dbfc891c36b644721d726f825bc3e
AAA <shurozka333@mail.com>
2022-08-09 09:13:19 +0300

    add DZ1-8

---
+++ Lesson1/DZ1-8/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.WriteLine("Введите число ");
+int number = int.Parse(Console.ReadLine());
+
+int count = 2;
+Console.WriteLine("Четные числа ");
+
+while (count < number)
+{
+    Console.WriteLine(count);
+    count+=2;
+}    
