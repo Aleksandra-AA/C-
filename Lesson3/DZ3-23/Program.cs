40d094d989f9ffbd22e24595dcf24af0275a3c9f
AAA <shurozka333@mail.com>
2022-08-13 14:16:40 +0300

    add DZ3-23

---
+++ Lesson3/DZ3-23/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.WriteLine("Введите число N:");
+int number = int.Parse(Console.ReadLine());
+
+int count = 1;
+Console.WriteLine("Таблица кубов от 1 до N:");
+
+while (count <= number)
+{
+    double cube = Math.Pow(count, 3);
+    Console.WriteLine(cube);
+    count += 1;
+}
