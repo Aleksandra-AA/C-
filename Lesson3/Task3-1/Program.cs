ca235eca986603e15d0bc39a08ae51630f929404
AAA <shurozka333@mail.com>
2022-08-15 14:25:41 +0300

    add Task3-1, Task3-2

--- Lesson3/Task3-1/Program.cs
+++ Lesson3/Task3-1/Program.cs
@@ -1,3 +1,27 @@
-﻿// See https://aka.ms/new-console-template for more information
-// Console.WriteLine("Hello, World!");
+﻿Console.WriteLine("Введите координаты точки А ");
 
+int x = int.Parse(Console.ReadLine());
+Console.WriteLine("x = " + x);
+
+int y = int.Parse(Console.ReadLine());
+Console.WriteLine("y = " + y);
+
+if ( x > 0 && y > 0) 
+{
+    Console.WriteLine("Точка А лежит в первой четверти");
+}
+
+if ( x > 0 && y < 0) 
+{
+    Console.WriteLine("Точка А лежит в четвертой четверти: ");
+}
+
+if ( x < 0 && y < 0) 
+{
+    Console.WriteLine("Точка А лежит в третьей четверти");
+}
+
+if ( x < 0 && y > 0) 
+{
+    Console.WriteLine("Точка А лежит во второй четверти");
+}
---
+++ Lesson3/Task3-2/Program.cs
@@ -0,0 +1,21 @@
+﻿Console.WriteLine("Введите координаты точки А ");
+
+double xA = double.Parse(Console.ReadLine());
+Console.WriteLine("xA = " + xA);
+double yA = double.Parse(Console.ReadLine());
+Console.WriteLine("yA = " + yA);
+
+Console.WriteLine("Введите координаты точки B ");
+
+double xB = double.Parse(Console.ReadLine());
+Console.WriteLine("xB = " + xB);
+double yB = double.Parse(Console.ReadLine());
+Console.WriteLine("yB = " + yB);
+
+double distance(double xA, double yA, double xB, double yB)
+
+{
+    return Math.Sqrt(Math.Pow(xB - xA, 2) + Math.Pow(yB - yA, 2));
+}
+
+Console.WriteLine("Расстояние между точками А и В: " + distance(xA, xB, yA, yB));
